4f5ca81f633073060fe5e7c6e7362e18e1d78728
Ivan <vanyason@mail.ru>
2022-03-30 17:03:14 +0400

    Updated bot: 1. Added usd to amd 2. Deleted params 3. Made search specific only for Yerevan 4. Rewrote errorhandling - made program`s work infinite

--- listam-parser.py
+++ listam-parser.py
@@ -18,42 +18,48 @@ import telegram_send
 import argparse
 
 
+def usdToAmd(price):
+    return price * 500
+
+
 def findUniqueElements(oldList, newList):
     return list(set(newList) - set(oldList))
 
+# currency = 1 means USD, 0 - AMD
+def getAdsFromListAm(price, roomsAmount, currency):
+    if currency != 1:
+        price = usdToAmd(price)
 
-def getParams(price, roomsAmount):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    listAmUrl = f"https://www.list.am/ru/category/56/1?pfreq=1&po=1&n=0&price2={price}&crc=1&_a4={roomsAmount}"
-    listAmUrlOriginal = f"https://www.list.am/category/56?pfreq=1&po=1&n=0&price2={price}&crc=1&_a4={roomsAmount}"
-    maxErrorsToStop = 10
-    return {'headers': headers, 'listAmUrl': listAmUrl, 'listAmUrlOriginal': listAmUrlOriginal, 'maxErrorsToStop': maxErrorsToStop}
-
-
-def getAdsFromListAm(params):
+    listAmUrl = f"https://www.list.am/ru/category/56/1?cmtype=1&pfreq=1&po=1&n=1&price2={price}&crc={currency}&_a4={roomsAmount}"
+    listAmUrlOriginal = f"https://www.list.am/category/56?cmtype=1&pfreq=1&po=1&n=1&price2={price}&crc={currency}&_a4={roomsAmount}"
     pages = []
     pageNum = 1
-    url = params['listAmUrl']
 
     # get all the html pages possible
     # when pages end - list am will respomd with the original url
     while True:
-        resp = requests.get(url, headers=params['headers'])
-        if (params['listAmUrlOriginal'] == resp.url):
+        try:
+            resp = requests.get(listAmUrl, headers=headers)
+        except Exception as ex:
+            raise ConnectionError(f'Some requests problem: info {ex}')
+
+        if (listAmUrlOriginal == resp.url):
             break
         if (resp.status_code != 200):
             raise ConnectionError('list am didn`t return code 200')
+
         pages.append(resp.text)
-        url = url.replace(f'/{pageNum}?', f'/{pageNum + 1}?')
+        listAmUrl = listAmUrl.replace(f'/{pageNum}?', f'/{pageNum + 1}?')
         pageNum = pageNum + 1
 
     # exctract links from the pages
-    links = []
+    links = set()
     for page in pages:
         soup = BeautifulSoup(page, 'lxml')
         for link in soup.find_all('a'):
             if "/item/" in str(link):
-                links.append('https://www.list.am' + link.get('href'))
+                links.add('https://www.list.am' + link.get('href'))
 
     return links
 
@@ -70,13 +76,11 @@ if __name__ == "__main__":
         quit()
 
     # Setup params
-    errorCounter = 0
     refreshRate = 60
     price = args.price
     roomsAmout = args.rooms
-    dbFileName = f'db_price_{price}_rooms_{roomsAmout}.txt'
-    params = getParams(price, roomsAmout)
-    loggingFileName = f'logs_price_{price}_rooms_{roomsAmout}.log'
+    dbFileName = f'db_price_{price}USD_rooms_{roomsAmout}.txt'
+    loggingFileName = f'logs_price_{price}USD_rooms_{roomsAmout}.log'
     logging.basicConfig(format='%(asctime)s - %(levelname)s:%(message)s',
                         datefmt='%m/%d/%Y %I:%M:%S %p',
                         filename=loggingFileName,
@@ -88,9 +92,11 @@ if __name__ == "__main__":
     while True:
         try:
             # Get data from List am
-            links = getAdsFromListAm(params)
+            links = set()
+            links.update(getAdsFromListAm(price, roomsAmout, 1))
+            links.update(getAdsFromListAm(price, roomsAmout, 0))
 
-            # If db doesn`t exist - create and repeat
+            # If db doesn`t exist - create
             if not os.path.exists(dbFileName):
                 logging.info(f'BD doesn`t exist. Creating: {dbFileName}')
                 with open(dbFileName, "w") as db:
@@ -114,10 +120,5 @@ if __name__ == "__main__":
         except Exception as ex:
             logging.exception(ex)
             print(ex)
-
-            errorCounter = errorCounter + 1
-            if (errorCounter >= params['maxErrorsToStop']):
-                logging.critical('Some serious shit happens. IT`S TIME TO STOP')
-                break
 
         time.sleep(refreshRate)
